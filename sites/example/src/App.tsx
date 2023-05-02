bc3b39fd54210f0adfea1df89f955ed776d834d8
Tom Erik Støwer <testower@gmail.com>
2023-05-02 14:01:47 +0200

    Add scaffolding for new package

---
+++ sites/example/src/App.tsx
@@ -0,0 +1,13 @@
+import {Foo} from '@entur/auth-provider';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Foo />
+    </>
+  )
+}
+
+export default App
--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react'
-import { AuthContext } from './common'
-
-export { AuthProvider as default } from './AuthProvider'
-
-export const useAuth = () => useContext(AuthContext)
